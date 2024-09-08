1d25a47808b9ba6f633af4e1b6daa1f6a6123b27
LeOzInNDF <leo_albano@hotmail.com>
2024-09-08 00:40:47 -0300

    🎨  Update-code🎨

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image } from 'react-native';
+import Capa from './assets/capa-tecnologia.png'
+import Logo from './assets/leo.png' // Load a PNG image
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.capa} source={Capa} />
+      
+    <View style={styles.boxDescricao}>
+      <Image style={styles.logo} source={Logo}  />
+      
+      <Text style={styles.titulo}>Leonardo Siqueira</Text>
+      <Text style={styles.cargo}>Software Engineer Full Stack Developer!</Text>
+      <Text  style={styles.cargo}>Kotlin | Javascript | Typescript | Dart | Angular | Flutter | Git | Github</Text>
       <StatusBar style="auto" />
+    </View>
     </View>
   );
 }
@@ -14,7 +24,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  capa: {
+    flex: 1,
+  },
+
+  boxDescricao: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  
+  logo: {
+
+    width: 100,
+    height: 100,
+    marginTop: 20,
+    marginBottom: 20,
+    borderRadius: 50,
+  },
+
+  titulo: {
+    textAlign: 'center',
+    marginHorizontal: 15,
+    fontSize: 16
+  }
 });
